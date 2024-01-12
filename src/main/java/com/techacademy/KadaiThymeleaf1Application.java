1ba1620129ad8f0ba8ff1eaa7731577e9d454508
takumi-sashiki <tkm.ssk.wilson.1211@gmail.com>
2024-01-12 17:52:57 +0900

    2024/1/12 課題

---
+++ src/main/java/com/techacademy/KadaiThymeleaf1Application.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadaiThymeleaf1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KadaiThymeleaf1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/techacademy/KadaiThymeleaf1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KadaiThymeleaf1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
